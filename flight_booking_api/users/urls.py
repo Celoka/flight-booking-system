b796aea99c2c9596e9eb70e43658692cec0cec6e
Eloka <eloka.chima@andela.com>
2019-02-27 12:48:08 +0100

    feat(photo): user upload photo
    - include extra fields in the user model
    - create endpoints to upload and delete picture
    [finishes #164002214]

--- flight_booking_api/users/urls.py
+++ flight_booking_api/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RegisterUserView, LoginView
+from .views import (RegisterUserView, 
+                    LoginView, 
+                    ImageUploadViewSet)
 
 urlpatterns = [
     path('auth/register/', RegisterUserView.as_view(), name="auth-register"),
     path('auth/login/', LoginView.as_view(), name="auth-login"),
+    path('upload/', ImageUploadViewSet.as_view(), name="file-upload")
 ]
