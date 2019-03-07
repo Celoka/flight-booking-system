bdb7e8afd8f9f3a03844b50b8d66d6a1db9b4205
Eloka <eloka.chima@andela.com>
2019-03-07 23:17:56 +0100

    Updated static root

--- flight_booking_api/flight_booking_api/settings/base.py
+++ flight_booking_api/flight_booking_api/settings/base.py
@@ -19,6 +19,7 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = 'e4gmk)8q80t^b%zf(tu^z)hdd$*femf!*c-uafw#x5!#c9#)t='
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
