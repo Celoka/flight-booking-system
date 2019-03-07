cb35ecc50dc1b28c594df328d9d6adb087519b22
Eloka <eloka.chima@andela.com>
2019-03-07 23:13:27 +0100

    Updated static root

--- flight_booking_api/flight_booking_api/settings/base.py
+++ flight_booking_api/flight_booking_api/settings/base.py
@@ -111,7 +111,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -158,6 +157,8 @@ JWT_AUTH = {
     'JWT_AUTH_COOKIE': None,
 }
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
