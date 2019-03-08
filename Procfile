076585cd6a959748792f4f63096ad04ab2b56a12
Eloka <eloka.chima@andela.com>
2019-03-08 23:12:19 +0100

    update wsgi.py to have correct module

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn flight_booking_api.flight_booking_api.wsgi --log-file -
+web: cd flight_booking_api && gunicorn flight_booking_api.wsgi --log-file -
--- flight_booking_api/flight_booking_api/settings/base.py
+++ flight_booking_api/flight_booking_api/settings/base.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
