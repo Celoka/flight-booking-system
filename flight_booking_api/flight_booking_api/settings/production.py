d63c52bfa796489f03ac03d6a509f40d90f28031
Eloka <eloka.chima@andela.com>
2019-03-09 12:51:43 +0100

    update settings for deployment

--- flight_booking_api/flight_booking_api/settings/production.py
+++ flight_booking_api/flight_booking_api/settings/production.py
@@ -11,8 +11,5 @@ DEBUG = False
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DATABASES['default'] = dj_database_url.config(
-    default={
-        'DATABASE_URL':os.getenv('DATABASE_URL'),
-        'REDISCLOUD_URL': os.getenv('REDISCLOUD_URL')
-        },conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'),
+                                            conn_max_age=600, ssl_require=True)
