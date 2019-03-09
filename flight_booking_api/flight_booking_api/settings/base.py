e9a44dfb4699a4c7902f295ae94a85ebbeb2c522
Eloka <eloka.chima@andela.com>
2019-03-09 12:51:43 +0100

    update settings for deployment

--- flight_booking_api/flight_booking_api/settings/base.py
+++ flight_booking_api/flight_booking_api/settings/base.py
@@ -166,8 +166,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # CELERY STUFF
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+BROKER_URL = os.getenv('REDISCLOUD_URL')
+CELERY_RESULT_BACKEND = os.getenv('REDISCLOUD_URL')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
