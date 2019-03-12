a2a987a32b1b0f4b734214b6ad7498542b968bce
Eloka <eloka.chima@andela.com>
2019-03-12 23:40:51 +0100

    add more endpoint for upload file and index route

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import (RegisterUserView, 
                     LoginView, 
-                    ImageUploadViewSet)
+                    ImageUploadViewSet,
+                    index)
 
 urlpatterns = [
+    path('', index, name="home-route"),
     path('auth/register/', RegisterUserView.as_view(), name="auth-register"),
     path('auth/login/', LoginView.as_view(), name="auth-login"),
-    path('user/upload/', ImageUploadViewSet.as_view(), name="file-upload")
+    path('user/upload/', ImageUploadViewSet.as_view(), name="file-upload"),
+    path('user/upload/<int:pk>/', ImageUploadViewSet.as_view(), name="file-upload-detail")
 ]
